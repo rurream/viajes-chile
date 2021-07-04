664ec2d38bebe803c33410e19118b8dcfbe5833e
Rodrigo Urrea M <rurream@gmail.com>
2021-07-04 13:19:51 -0400

    maqueta lista

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,29 @@ $(document).ready(function() {
         $(this).addClass('active');
    });
 });
+
+// $(".navbar-nav .nav-item a").on("click", function (e) {
+//     e.preventDefault();
+//     const href = $(this).attr("href");
+//     $("html, body").animate({ scrollTop: $(href).offset().top }, 1800);
+//   });
+
+(function () {
+    var qs,
+      js,
+      q,
+      s,
+      d = document,
+      gi = d.getElementById,
+      ce = d.createElement,
+      gt = d.getElementsByTagName,
+      id = "typef_orm",
+      b = "https://embed.typeform.com/";
+    if (!gi.call(d, id)) {
+      js = ce.call(d, "script");
+      js.id = id;
+      js.src = b + "embed.js";
+      q = gt.call(d, "script")[0];
+      q.parentNode.insertBefore(js, q);
+    }
+  })();
